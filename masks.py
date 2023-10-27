43d1eb5f9dd7383bab2806553337f77f0f84af97
Gelena Bezusco <helenatcitok@gmail.com>
2023-10-27 08:06:36 +0100

    Initial commit

---
+++ masks.py
@@ -0,0 +1,9 @@
+def mask_card_number(card_number: str) -> str:
+    masked_number = f'{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}'
+    return masked_number
+
+
+def mask_account_number(account_number: str) -> str:
+    masked_number = f'**{account_number[-4:]}'
+    return masked_number
+
